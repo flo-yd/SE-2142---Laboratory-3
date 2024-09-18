d7ab74ae0b9ac2b2ca92ba18a5a195e65ee0c05b
floyd <floydmatthew.torrechilla-21@cpu.edu.ph>
2024-09-18 15:47:37 +0800

    branch2 feature

---
+++ featurefile.py
@@ -0,0 +1,12 @@
+# insert feature from branch 1 here:
+
+
+# insert feature from branch 2 here:
+def hiluu():
+  text = "hiluu"
+  print(text)
+
+# to be fixed by branch 3:
+def randomFunction()
+  randomText = hello
+  print(randomText)
